2f8d2a6c937899528ce95a5f07d5f0f7c6873847
standoski <standoskibartho@gmail.com>
2022-11-04 22:14:00 +0100

    'typescript'

--- utils/axios.tsx
+++ utils/axios.tsx
@@ -2,14 +2,14 @@ import axios from "axios";
 import https from 'https'
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "naijacinemas.netlify.app/",
   timeout: 360000,
   httpsAgent: new https.Agent({ keepAlive: true }),
 });
 
 
 export const tokenInstance = (TOKEN:string) => axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "naijacinemas.netlify.app/",
   timeout: 360000,
   httpsAgent: new https.Agent({ keepAlive: true }),
   headers: {  token: `Bearer ${TOKEN}` },
