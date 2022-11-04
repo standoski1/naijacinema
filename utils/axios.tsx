36ee62ec730295b4d72a7877597e8fcb59f58c80
standoski <standoskibartho@gmail.com>
2022-11-04 23:09:02 +0100

    'typescript'

--- utils/axios.tsx
+++ utils/axios.tsx
@@ -2,13 +2,17 @@ import axios from "axios";
 import https from 'https'
 
 export const axiosInstance = axios.create({
-  baseURL: "https://naijacinemas.netlify.app/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "https://naijacinemas.herokuapp.com/",
+  timeout: 360000,
   httpsAgent: new https.Agent({ keepAlive: true }),
 });
 
 
 export const tokenInstance = (TOKEN:string) => axios.create({
-  baseURL: "https://naijacinemas.netlify.app/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "https://naijacinemas.herokuapp.com/",
+  timeout: 360000,
   httpsAgent: new https.Agent({ keepAlive: true }),
   headers: {  token: `Bearer ${TOKEN}` },
 });
